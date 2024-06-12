4bb404bdd1befdc6d94285bf1caffc6b37985a93
Tcheetox <tcheetox@gmail.com>
2024-06-12 16:15:47 +0200

    kekw

--- Portfolio-Box/Controllers/RemoteFileController.cs
+++ Portfolio-Box/Controllers/RemoteFileController.cs
@@ -12,7 +12,7 @@ using Portfolio_Box.Models.Users;
 namespace Portfolio_Box.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("drop2vlar/api/[controller]")]
     public partial class RemoteFileController : ControllerBase
     {
         private readonly IFileRepository _fileRepository;
